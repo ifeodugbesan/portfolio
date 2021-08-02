37e31e622c5c650d0b84ac6cecaef1b34cc32b3e
Ife Odugbesan <ifeodugbesan@yahoo.com>
2021-08-02 22:28:34 +0100

    implemented dark mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :mode
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  def mode
+    cookies[:dark_mode] == "true" ? @dark_mode = true : @dark_mode = false
   end
 end
